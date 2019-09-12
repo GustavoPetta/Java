58df383e87e10c1a2764e4b79721fbb1efe39dee
Gustavo Petta da Silva <gustavo.silva@SERVICE.COM.BR>
2019-09-12 16:34:19 -0300

    Java

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into previsao (dia,minima,maxima,humidade,descricao) values ('SEXTA',15.3,20,35.1,'QUENTE');
+insert into previsao (dia,minima,maxima,humidade,descricao) values ('SABADO',14,18,40,'FRIO');
+insert into previsao (dia,minima,maxima,humidade,descricao) values ('DOMINGO',13,19,35.1,'NUBLADO');
+
+insert into usuario (id, login, senha) values (1, 'admin', 'admin');
